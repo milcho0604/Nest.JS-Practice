4c060a524e26dc231da6a722cea1ea54f41d2c83
milcho0604 <milcho0604@gmail.com>
2026-02-08 13:51:37 +0900

    init commit

---
+++ src/users/users.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UsersService {
+  private users = [];
+
+  findAll() {
+    return this.users;
+  }
+
+  findOne(id: string) {
+    return this.users.find((user) => user.id === id);
+  }
+
+  create(body: { name: string; email: string }) {
+    const user = { id: String(this.users.length + 1), ...body };
+    this.users.push(user);
+    return user;
+  }
+}
