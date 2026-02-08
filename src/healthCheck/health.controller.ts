6f7382db75018ee3fa3fd86a54efcd01fb8170de
milcho0604 <milcho0604@gmail.com>
2026-02-08 14:25:14 +0900

    feat: healthcheck controller post

--- src/healthCheck/health.controller.ts
+++ src/healthCheck/health.controller.ts
@@ -1,9 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('health') // health 경로 prefix 사용하는 컨트롤러
 export class HealthCheckController {
-  @Get()
-  findAll(): string {
+  @Get('check')
+  healCheckController(): string {
     return 'HealthCheck Complete';
+  }
+
+  @Post('server')
+  healthCheckServerController(): string {
+    const msg = 'ok';
+    if (msg) {
+      return 'server checking: ' + msg;
+    } else {
+      return 'server checking: ' + 'down';
+    }
   }
 }
